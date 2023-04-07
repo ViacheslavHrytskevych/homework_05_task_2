dd4e7fcd1ff23b57fec1bc49986f6fdfefc3e703
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-04-07 23:36:26 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] number = {3, 5, 155, 11, 16, 20};
+
+        List<Integer> list = toList(number);
+
+        System.out.println(list);
+    }
+
+    public static List<Integer> toList(int[] number) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : number) {
+            list.add(i);
+        }
+        return list;
+    }
+
+
 }
